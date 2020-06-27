b9cda1cb8d4d01266631d4e2ba9f72afbbd3d30f
Evgeny <metelkin.evgen@yandex.ru>
2020-06-27 22:29:49 +0300

    Fix getAbsolutePath and test for it

--- Global/Test/testfiledirutils.cpp
+++ Global/Test/testfiledirutils.cpp
@@ -8,5 +8,7 @@ TestFileDirUtils::TestFileDirUtils()
 
 void TestFileDirUtils::getAbsolutePath()
 {
-    QCOMPARE(FileDirUtils::getAbsolutePath(""), QString("D:/MyProg/QtCreator/build-PerfectLive-Desktop_Qt_5_11_3_MinGW_32bit-Debug"));
+    QCOMPARE(FileDirUtils::getAbsolutePath(""), QString("D:/MyProg/QtCreator/build-PerfectLive-Desktop_Qt_5_11_3_MinGW_32bit-Debug/"));
+    QCOMPARE(FileDirUtils::getAbsolutePath("fileName"), QString("D:/MyProg/QtCreator/build-PerfectLive-Desktop_Qt_5_11_3_MinGW_32bit-Debug/fileName"));
+    QCOMPARE(FileDirUtils::getAbsolutePath("*@h123lhn1o-dfk"), QString("D:/MyProg/QtCreator/build-PerfectLive-Desktop_Qt_5_11_3_MinGW_32bit-Debug/*@h123lhn1o-dfk"));
 }
